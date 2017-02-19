9b9d02e11cf39b7450c25380358e7f22ba494bfe
Windendless <my8159@gmail.com>
2017-02-19 22:08:55 +0800

    Revert: refine code

--- src/utils.h
+++ src/utils.h
@@ -189,11 +189,12 @@ extern int use_syslog;
 
 #ifdef __MINGW32__
 
-#define posix_memalign(p, a, s) (((*(p)) = _aligned_malloc((s), (a))), *(p) ?0 :errno)
-
 #ifdef ERROR
 #undef ERROR
 #endif
+
+#define posix_memalign(p, a, s) (((*(p)) = _aligned_malloc((s), (a))), *(p) ?0 :errno)
+
 #define ERROR(s) ss_error(s)
 
 #else
